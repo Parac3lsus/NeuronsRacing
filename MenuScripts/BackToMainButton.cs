3aef1f1aa14c62ebb8fe55321ae6a81582c2f4b1
Parac3lsus <agustinguru@hotmail.com>
2023-02-27 17:03:36 -0300

    Added MenuScripts folder with BackToMain button script

---
+++ MenuScripts/BackToMainButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainButton : MonoBehaviour
+{
+	[SerializeField]
+	private string mainMenu = "MainMenu";
+	public void BackToMain()
+	{
+		SceneManager.LoadScene(mainMenu);
+	}
+}
