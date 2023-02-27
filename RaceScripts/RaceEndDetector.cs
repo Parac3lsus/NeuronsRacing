e14367263d7fe3b237b61ea45aab4b968a87aede
Parac3lsus <agustinguru@hotmail.com>
2023-02-27 13:12:10 -0300

    Added RaeEndetector script to detect when the first 3 players ended the race and RaceFinalPositionsUI that will be in charge of showing the final positions

---
+++ RaceScripts/RaceEndDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceEndDetector : MonoBehaviour
+{
+    private RaceFinalPositionsUI positionsUI;
+    private int carsFinished = 0;
+
+    private void OnEnable()
+    {
+        ProgressDetector.OnRaceFinished += CheckRaceFinished;
+    }
+
+	private void Start()
+	{
+        positionsUI = GetComponent<RaceFinalPositionsUI>();
+	}
+
+	private void CheckRaceFinished()
+    {
+        carsFinished += 1;
+        if(carsFinished >= 3)
+		{
+            positionsUI.ShowFinalPositions();
+		}
+        Debug.Log("Check race Finished");
+    }
+    private void OnDisable()
+    {
+        ProgressDetector.OnRaceFinished -= CheckRaceFinished;
+    }
+}
---
+++ RaceScripts/RaceFinalPositionsUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceFinalPositionsUI : MonoBehaviour
+{
+	private RaceManager raceManager;
+	private List<ProgressDetector> finalPositions;
+
+	private void Start()
+	{
+		raceManager = GetComponent<RaceManager>();
+	}
+
+
+	public void ShowFinalPositions()
+	{
+		finalPositions = raceManager.GetPositions();
+
+		for(int i = 0; i < finalPositions.Count; i++)
+		{
+			Debug.Log("Position: " + i+1 + "Car Name: " + finalPositions[i].carName);
+		}
+	}
+}
