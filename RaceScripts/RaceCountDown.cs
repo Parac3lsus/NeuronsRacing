4c91913611917a93e3493dc4ac658918d9bf7e81
Parac3lsus <agustinguru@hotmail.com>
2023-02-28 12:20:18 -0300

    Race Countdown fully implemented

--- RaceScripts/RaceCountDown.cs
+++ RaceScripts/RaceCountDown.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class RaceCountDown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private GameObject redLight;
+	[SerializeField]
+	private GameObject yellowLight;
+	[SerializeField]
+	private GameObject greenLight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private CarDrive[] carDriveControllers;
+
+
+	private void Start()
+	{
+		DisableLights();
+		carDriveControllers = FindObjectsOfType<CarDrive>();
+		SetCarsActiveState(false);
+		StartCoroutine(CountDownRoutine());
+		//SetCarsActiveState(true);
+	}
+
+	private void SetCarsActiveState(bool enabled)
+	{
+		foreach(CarDrive driveController in carDriveControllers)
+		{
+			driveController.carEnabled =enabled;
+			driveController.GetComponent<StuckDetector>().enabled = enabled;
+		}
+	}
+
+	private void DisableLights()
+	{
+		redLight.SetActive(false);
+		yellowLight.SetActive(false);
+		greenLight.SetActive(false);
+	}
+
+
+	IEnumerator CountDownRoutine()
+	{
+		yield return new WaitForSeconds(1.5f);
+		redLight.SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		yellowLight.SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		greenLight.SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		DisableLights();
+		SetCarsActiveState(true);
+	}
 }
