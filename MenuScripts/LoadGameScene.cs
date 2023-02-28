e6aee6363f6f5bef23901ba91ba3c70f9df6785b
Parac3lsus <agustinguru@hotmail.com>
2023-02-28 11:21:17 -0300

    Implemented LoadGameScene script for main menu and started implementation of RaceCountdown script for race start countdown

---
+++ MenuScripts/LoadGameScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameScene : MonoBehaviour
+{
+	[SerializeField]
+	private string playerRace = "PlayerRace";
+	[SerializeField]
+	private string iaRace = "IACompetition";
+	public void LoadSlectedScene(int selectedScene)
+	{
+		if (selectedScene == 1)
+			SceneManager.LoadScene(playerRace);
+		if (selectedScene == 2)
+			SceneManager.LoadScene(iaRace);
+	}
+}
