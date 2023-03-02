228e53c41e76bf1ede2a4101a42847e3e8f3bcbd
Parac3lsus <agustinguru@hotmail.com>
2023-03-02 16:53:17 -0300

    Added model rotator to be used be the main menu Scene

---
+++ MenuScripts/ModelRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rot * Time.deltaTime);
+    }
+}
