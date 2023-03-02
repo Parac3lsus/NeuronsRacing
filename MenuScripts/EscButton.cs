dc78c1e1653411e8d435e93052a3e505dae6c456
Parac3lsus <agustinguru@hotmail.com>
2023-03-02 14:58:42 -0300

    Implemented Escape button to return to main

---
+++ MenuScripts/EscButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscButton : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject ExitMenu;
+
+	private RaceManager raceManager;
+	private LoadGameScene sceneLoader;
+
+	private void Start()
+	{
+		raceManager = FindObjectOfType<RaceManager>();
+		sceneLoader = GetComponent<LoadGameScene>();
+	}
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			ExitMenu.SetActive(true);
+			Time.timeScale = 0.0f;
+		}
+	}
+
+	public void SelectedOption(int opt)
+	{
+		if (opt == 1)
+		{
+			sceneLoader.LoadSlectedScene(0);
+		}
+		else
+		{
+			ContinueExcecution();
+		}
+	}
+
+	public void ContinueExcecution()
+	{
+		ExitMenu.SetActive(false);
+		Time.timeScale = raceManager.timeScale;
+	}
+}
